edb2e760fd75252fa28da5650e1042125b36356d
GabDevCode <kr4v3n27@gmail.com>
2025-10-26 17:21:29 -0600

    Se mapeo la data y se creo la clase mapper

--- lib/domain/infrestructure/datasources/themovies_datasource.dart
+++ lib/domain/infrestructure/datasources/themovies_datasource.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:movie_pedia/config/constants/environment.dart';
 import 'package:movie_pedia/domain/datasources/movies_datasources.dart';
 import 'package:movie_pedia/domain/entities/movie.dart';
+import 'package:movie_pedia/domain/infrestructure/mappers/movie_mapper.dart';
+import 'package:movie_pedia/domain/infrestructure/models/moviedb/moviedb_response.dart';
 
 /// Clase creada para interacturar con la clase Movie Datasource
 
@@ -20,7 +22,14 @@ class ThemoviesDatasource extends MoviesDatasources {
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
 
-    final List<Movie> movies = [];
+    final movieDbResponse = TheMovieDb.fromJson(response.data); // recibimos el Json
+
+    // Mapeamos 
+
+    final List<Movie> movies = movieDbResponse.results 
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieTheMovieDbEntity(moviedb))
+        .toList();
 
     return movies;
   }
---
+++ lib/domain/infrestructure/mappers/movie_mapper.dart
@@ -0,0 +1,28 @@
+// Clase generada para leer diferentes modelos y crear una entidad
+// Objetivo principal: Funcionar como una capa de proteccion para recibir la data de la APi, con nuestra aplicacion
+
+import 'package:movie_pedia/domain/entities/movie.dart';  
+import 'package:movie_pedia/domain/infrestructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieTheMovieDbEntity(MovieMoviedb moviedb) => Movie(
+    adult: moviedb.adult,
+    backdropPath: (moviedb.backdropPath != '')
+        ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+        : 'https://uxwing.com/wp-content/themes/uxwing/download/web-app-development/image-not-found-icon.png',
+    genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+    id: moviedb.id,
+    originalLanguage: moviedb.originalLanguage,
+    originalTitle: moviedb.originalTitle,
+    overview: moviedb.overview,
+    popularity: moviedb.popularity,
+    posterPath: (moviedb.posterPath != '')
+    ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+    : 'no-poster',
+    releaseDate: moviedb.releaseDate,
+    title: moviedb.title,
+    video: moviedb.video,
+    voteAverage: moviedb.voteAverage,
+    voteCount: moviedb.voteCount,
+  );
+}
